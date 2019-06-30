a3daee0b456dbefe8694a5318e00aa387cea7fa2
Daniel Corcoran <danielc.92@outlook.com>
2019-07-01 09:57:58 +1000

    Added json response example

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 // Load node's http server
 const http = require('http');
+const fs = require('fs');
+
+let HTML = fs.readFileSync('./templates/index.html')
 
 /* 
 Create server with request and response
@@ -7,8 +10,21 @@ Writer headers and status code
 Send content in end()
 Listen on localhost port 3001 (port must be free)
 */
-http.createServer((request, response) => {
-    // set the headers
-    response.writeHead(200, {"Content-type": "text/plain"})
-    response.end('Hello world')	
-}).listen(3001, '127.0.0.1')
+const server = http.createServer((request, response) => {
+    // Set the headers
+    //response.writeHead(200, { "Content-type": "text/html" })
+    // Send HTML from template (index.html)
+    // response.end(HTML)
+    
+    response.writeHead(200, {"Content-type": "application/json"})
+    
+    const jsonResponse = JSON.stringify({
+        name: "daniel",
+        hobbys: ["programming", "cooking"]
+    })
+    age: 99,
+
+    response.end(jsonResponse)
+})
+
+server.listen(8000, '127.0.0.1')
