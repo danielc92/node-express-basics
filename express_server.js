d554aa49c70f814e149610b379205bf2ebbb6702
Daniel Corcoran <danielc.92@outlook.com>
2019-07-01 11:18:46 +1000

    Added a simple form

--- express_server.js
+++ express_server.js
@@ -9,8 +9,28 @@ app.use('/images', express.static(__dirname + '/public/images'))
 
 // How to send html response
 app.get('/', (request, response) => {
-    response.send(`<h1>Express Server</h1>
-    <img src="images/cloud-4273197_960_720.png"></img>`);
+    response.send(`
+    <!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Daniels Express Server</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.5/css/bulma.min.css">
+    <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
+  </head>
+  <body>
+  <section class="section">
+    <div class="container">
+      <h1 class="title">Express Server</h1>
+      <img src="images/cloud-4273197_960_720.png"></img>
+      <p class="subtitle">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia ducimus doloribus neque repellat et consequuntur distinctio mollitia iste, laboriosam, quas hic sit sunt exercitationem suscipit ullam maxime illum quibusdam fugit?</p>
+    </div>
+  </section>
+  </body>
+</html>
+
+    `);
 })
 
 
