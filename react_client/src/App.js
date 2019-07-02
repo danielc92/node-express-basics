c54e9b01038a780c5458d1c62f264d9dd4100c7b
Daniel Corcoran <danielc.92@outlook.com>
2019-07-02 10:21:42 +1000

    Added Cors headers to express backend, added a render for members state data in react frontend

--- react_client/src/App.js
+++ react_client/src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  
+  state = {
+    members: []
+  }
+
+  componentDidMount () {
+    const url = 'http://localhost:8000/api/members'
+    fetch(url,
+    {
+        "Content-type": "application/json", 
+        "Accept": "application/json"
+    })
+    .then(response => response.json())
+    .then(data => this.setState({members: data.members}))
+    .catch(error => console.log(`An error has occured in the intial api call ${error}`))
+  }
+
+  render() {
+    const boxStyle = {
+                        border: '1px solid black', 
+                        backgroundColor: 'lightblue', 
+                        padding: '1rem',
+                        margin: '1rem 1.2rem'
+                     }
+    return (
+      <div>
+        <h3>Members</h3>
+        <p>This react component is making an api call to the express server. On the express server we have to allow cross origin in the Headers sent with each response to the client (React) in order for it to work.</p>
+        {
+          this.state.members.map((item, index) => {
+            return (
+              index <= 10 ?
+              <div key={index} style={boxStyle}>
+                <h3>{item.username}</h3>
+                <p>{item.mail}</p>
+                <p>{item.address}</p>
+              </div>
+            :
+            null
+          )})
+        }
+      </div>
+    )
+  }
 }
-
-export default App;
